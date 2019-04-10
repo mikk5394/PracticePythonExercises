88e48fda8bee138ba53537d54aac0e3383000763
hackerMike <mikkel_olsen@live.dk>
2019-04-10 14:25:23 +0200

    First exercise

---
+++ characterinput1.py
@@ -0,0 +1,19 @@
+name = input('Type your name: ')
+age = ''
+repetitions = ''
+year = 2019
+
+while age != int and repetitions != int:
+    try:
+        age = int(input('Type your age: '))
+        repetitions = int(input('Type desired number of repetitions: '))
+        break
+    except ValueError:
+        print('input must be a number, try again!')
+
+
+print(f'{name}, you will turn 100 in year {(year-age)+100} \n' * repetitions)
+
+#Works aswell
+#for i in range(repetitions):
+#    print(f'{name}, you will turn 100 in year {(year-age)+100}')
