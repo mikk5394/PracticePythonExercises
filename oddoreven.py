954dce2c62f30e92dd2aa9002f4231b80b04f70c
hackerMike <mikkel_olsen@live.dk>
2019-04-10 15:10:13 +0200

    exercise2done

--- oddoreven.py
+++ oddoreven.py
@@ -2,7 +2,19 @@
 
 number = int(input('Type a number: '))
 
-if number %2 == 0:
+if number == 4:
+    print("4 is the special number")
+elif number %2 == 0:
     print('Your number is even')
 else:
     print('Your number is odd')
+
+#extras
+
+num = int(input('Type a number: '))
+check = int(input('Type a number: '))
+
+if num %check == 0:
+    print('Check divides evenly into num')
+else:
+    print('Check does not divide evenly into num')
