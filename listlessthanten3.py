a1f8e17c56dd97f7156920ec6e83953139f496da
hackerMike <mikkel_olsen@live.dk>
2019-04-11 18:18:10 +0200

    test

--- listlessthanten3.py
+++ listlessthanten3.py
@@ -1,4 +1,3 @@
-
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 for i in a:
@@ -8,13 +7,25 @@ for i in a:
 
 #extra1
 
-list = []
+newList = []
 
 for i in a:
     if i < 5:
-        list.append(i)
+        newList.append(i)
 
-print(list)
+print(newList)
+
 
 #extra2
 
+newList2 = [n for n in a if n < 5]
+print(newList)
+
+
+#extra3
+
+inp = int(input('At which number should the list cut? '))
+
+newList3 = [n for n in a if n < inp]
+print(newList3)
+        
